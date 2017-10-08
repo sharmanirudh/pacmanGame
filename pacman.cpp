46b2a266aea94ec3cb6539444add7bffbf3ef7b4
Anirudh Sharma <sharmanirudh27@gmail.com>
2017-10-08 15:04:14 +0000

    find ghost to pacman path and move ghost. BUG:- Since in the moveGhost() we are finding first path to pacman form current ghost position and then moving ghost to only the position of index 1 of pathToCatchPacman, there arises a situation when the path from one point to pacman has index 1 as the position of the previous position from where the ghost has moved earlier. Therefore ghost keeps on moving in a loop till the pacman moves in such a point where the pathToCatchPacman's index 1 is different.

--- pacman.cpp
+++ pacman.cpp
@@ -15,10 +15,12 @@ struct arrayPassByValue
 {
 	pos path[MAT_MAX * MAT_MAX];
 };
-int a;
+pos pathToCatchPacman[MAT_MAX * MAT_MAX];
 
 int findPath(int matrix[][MAT_MAX], int visited[][MAT_MAX], pos currentPos, pos pacmanPos, arrayPassByValue pathToPacman, int size)
 {
+	static int flag = 0;
+	int i, j;
 	visited[currentPos.x][currentPos.y] = 1;
 	if(!matrix[currentPos.x][currentPos.y] || currentPos.x < 0 || currentPos.x >= MAT_MAX || currentPos.y < 0 || currentPos.y >= MAT_MAX)
 	{
@@ -26,52 +28,76 @@ int findPath(int matrix[][MAT_MAX], int visited[][MAT_MAX], pos currentPos, pos 
 		return 0;
 	}
 
-	for(int i = 0; i < MAT_MAX; i++)
+	/*for(i = 0; i < MAT_MAX; i++)
 	{
-		for(int j = 0; j < MAT_MAX; j++)
+		for(j = 0; j < MAT_MAX; j++)
 			cout<<visited[i][j]<<" ";
 		cout<<"\n";
 	}
 	delay(1000);
+	*/
 	pathToPacman.path[size] = currentPos;
 	size++;
 	if(currentPos.x == pacmanPos.x && currentPos.y ==  pacmanPos.y)
 	{
-		a = 1;
-		cout<<"DHAPPA\n";
+		//cout<<"DHAPPA\n";
 		for(i = 0; i < size; i++)
-			cout<<"("<<pathToPacman.path[i].x<<", "<<pathToPacman.path[i].y<<"), ";
-		getch();
-		exit(0);
+		{
+			//cout<<"("<<pathToPacman.path[i].x<<", "<<pathToPacman.path[i].y<<"), ";
+			pathToCatchPacman[i].x = pathToPacman.path[i].x;
+			pathToCatchPacman[i].y = pathToPacman.path[i].y;
+		}
+		return 1;
 	}
-	clrscr();
+	//clrscr();
 	arrayPassByValue pathUptilNow;
 	for(i = 0; i < size; i++)
 		pathUptilNow.path[i] = pathToPacman.path[i];
+	if(!flag && matrix[currentPos.x][currentPos.y + 1] && !visited[currentPos.x][currentPos.y + 1])
+	{
+		pos newPos = {currentPos.x, currentPos.y + 1};
+		findPath(matrix, visited, newPos, pacmanPos, pathUptilNow, size);
+	}
+	if(!flag && matrix[currentPos.x + 1][currentPos.y] && !visited[currentPos.x + 1][currentPos.y])
+	{
+		pos newPos = {currentPos.x + 1, currentPos.y};
+		findPath(matrix, visited, newPos, pacmanPos, pathUptilNow, size);
+	}
+	if(!flag && matrix[currentPos.x - 1][currentPos.y] && !visited[currentPos.x - 1][currentPos.y])
+	{
+		pos newPos = {currentPos.x - 1, currentPos.y};
+		findPath(matrix, visited, newPos, pacmanPos, pathUptilNow, size);
+	}
 	if(matrix[currentPos.x][currentPos.y - 1] && !visited[currentPos.x][currentPos.y - 1])
 	{
 		pos newPos = {currentPos.x, currentPos.y - 1};
 		findPath(matrix, visited, newPos, pacmanPos, pathUptilNow, size);
 	}
-	if(matrix[currentPos.x - 1][currentPos.y] && !visited[currentPos.x - 1][currentPos.y])
-	{
-		pos newPos = {currentPos.x - 1, currentPos.y};
-		findPath(matrix, visited, newPos, pacmanPos, pathUptilNow, size);
-	}
-	if(matrix[currentPos.x + 1][currentPos.y] && !visited[currentPos.x + 1][currentPos.y])
-	{
-		pos newPos = {currentPos.x + 1, currentPos.y};
-		findPath(matrix, visited, newPos, pacmanPos, pathUptilNow, size);
-	}
-	if(matrix[currentPos.x][currentPos.y + 1] && !visited[currentPos.x][currentPos.y + 1])
-	{
-		pos newPos = {currentPos.x, currentPos.y + 1};
-		findPath(matrix, visited, newPos, pacmanPos, pathUptilNow, size);
-	}
 	return 0;
 }
 
-void movePacman(int matrixOriginal[][MAT_MAX], pos pacmanPos)
+pos moveGhost(int matrix[][MAT_MAX], int matrixOriginal[][MAT_MAX], pos currentPos, pos pacmanPos)
+{
+	int visited[10][10] ={{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0},
+												{0,0,0,0,0,0,0,0,0,0}};
+	arrayPassByValue pathToPacman;
+	findPath(matrixOriginal, visited, currentPos, pacmanPos, pathToPacman, 0);
+	matrix[pathToCatchPacman[0].x][pathToCatchPacman[0].y] = 0;
+	matrix[pathToCatchPacman[1].x][pathToCatchPacman[1].y] = 5;
+	currentPos.x = pathToCatchPacman[1].x;
+	currentPos.y = pathToCatchPacman[1].y;
+	return currentPos;
+}
+
+void movePacman(int matrixOriginal[][MAT_MAX], pos pacmanPos, pos ghostCurrentPos)
 {
 	int matrix[10][10] = {{0,0,0,0,0,0,0,0,0,0},
 												{0,0,0,0,0,0,0,0,0,0},
@@ -82,9 +108,10 @@ void movePacman(int matrixOriginal[][MAT_MAX], pos pacmanPos)
 												{0,0,0,0,0,0,0,0,0,0},
 												{0,0,0,0,0,0,0,0,0,0},
 												{0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,0,0,0,0,0,0}};
+												{0,0,0,0,0,0,0,0,0,0}}, i, j;
 	while(1)
 	{
+		ghostCurrentPos = moveGhost(matrix, matrixOriginal, ghostCurrentPos, pacmanPos);
 		if(kbhit())
 		{
 			char ch = getch();
@@ -122,35 +149,36 @@ void movePacman(int matrixOriginal[][MAT_MAX], pos pacmanPos)
 						matrix[pacmanPos.x][++pacmanPos.y] = 1;
 					}
 				}
-				clrscr();
-				for(int i = 0; i < MAT_MAX; i++)
-				{
-					for(int j = 0; j < MAT_MAX; j++)
-						cout<<matrix[i][j]<<" ";
-					cout<<"\n";
-				}
 			}
 			else if(ch == 'q')
 				break;
 		}
+		clrscr();
+		for(int i = 0; i < MAT_MAX; i++)
+		{
+			for(int j = 0; j < MAT_MAX; j++)
+				cout<<matrix[i][j]<<" ";
+			cout<<"\n";
+		}
+		delay(1000);
 	}
 }
 
 int main()
 {
 	clrscr();
-	int matrix[10][10] = {{0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,0,0,0,0,0,0},
-												{0,0,0,0,1,1,1,0,0,0},
-												{0,0,0,0,1,1,1,0,0,0},
-												{0,0,0,0,1,0,0,0,0,0},
+	int matrix[10][10] = {{1,1,1,1,1,0,0,1,0,0},
+												{0,1,0,0,1,1,1,1,0,0},
+												{0,1,0,0,0,0,0,0,0,0},
+												{0,1,0,0,0,0,0,0,0,0},
+												{0,1,0,0,1,1,1,0,0,0},
+												{0,1,1,1,1,1,1,0,0,0},
+												{0,0,1,1,1,0,0,0,0,0},
 												{0,0,0,0,1,1,1,1,0,0},
 												{0,0,0,0,0,0,1,1,0,0},
 												{0,0,0,0,0,0,1,1,1,1}}, i, j;
 
-	int visited[10][10] ={{0,0,0,0,0,0,0,0,0,0},
+	/*int visited[10][10] ={{0,0,0,0,0,0,0,0,0,0},
 												{0,0,0,0,0,0,0,0,0,0},
 												{0,0,0,0,0,0,0,0,0,0},
 												{0,0,0,0,0,0,0,0,0,0},
@@ -161,15 +189,17 @@ int main()
 												{0,0,0,0,0,0,0,0,0,0},
 												{0,0,0,0,0,0,0,0,0,0}};
 	arrayPassByValue pathToPacman;
+	*/
 	/*for(i = 0; i < MAT_MAX; i++)
 	{
 		for(j = 0; j < MAT_MAX; j++)
 			cout<<matrix[i][j]<<" ";
 		cout<<"\n";
 	}*/
-	pos currentPos = {4, 4}, pacmanPos = {9, 9};
-	//findPath(matrix, visited, currentPos, pacmanPos, pathToPacman, 0);
-	movePacman(matrix, pacmanPos);
+	pos ghostCurrentPos = {0, 0};
+	pos pacmanPos = {9, 9};
+	//findPath(matrix, visited, ghostCurrentPos, pacmanPos, pathToPacman, 0);
+	movePacman(matrix, pacmanPos, ghostCurrentPos);
 	getch();
 	return 0;
 }
